01f5318af73c949ed7017c3c9d78011cd1dd2896
joan-kii <joan.sb81@gmail.com>
2021-07-10 17:21:42 +0200

    elimina gulp autoprefixer

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var autoprefixer = require('gulp-autoprefixer');
-
-gulp.task('run', async function() {
-  gulp.src('./styles/*.css')
-    .pipe(autoprefixer())
-    .pipe(gulp.dest('build'))
-});
